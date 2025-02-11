4bc382b53c4298fff02d993a09027fe082b5c0db
tungduong150105 <tungduong150105@gmail.com>
2025-02-11 20:31:57 +0700

    Initial frontend

---
+++ frontend/app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#0a7ea4",
+        headerShown: false,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+    </Tabs>
+  );
+}
